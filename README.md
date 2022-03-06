c46f759097a42b4217247a344384966f50984588
dymafr <erwan1309@hotmail.com>
2022-03-06 13:09:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-jh6xn2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-jh6xn2)
---
+++ src/components/Cart/CartProduct.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { ProductInterface } from '@/interfaces';
+
+defineProps<{
+  product: ProductInterface;
+}>();
+
+const emit = defineEmits<{
+  (e: 'removeProductFromCart', productId: number): void;
+}>();
+</script>
+
+<template>
+  <div class="mb-10 p-10 d-flex flex-row align-items-center product">
+    <strong class="mr-10">{{ product.title }}</strong>
+    <span class="mr-10">Prix : {{ product.price }}€</span>
+    <button
+      class="btn btn-danger"
+      @click="emit('removeProductFromCart', product.id)"
+    >
+      Supprimer
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.product {
+  border: var(--border);
+  border-radius: var(--border-radius);
+  background-color: var(--gray-1);
+}
+</style>
